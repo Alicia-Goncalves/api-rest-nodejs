328bf67c34c403ff09153c84e7bc80620f186fa8
Alicia Gonçalves <aliciagoncalvesp@gmail.com>
2026-01-03 19:45:17 -0300

    feat: add session cookies for transactions

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import 'dotenv/config'
+
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import crypto from 'node:crypto'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify ()
 
+app.register(cookie) 
 app.register(transactionsRoutes)
 
 app.listen({
